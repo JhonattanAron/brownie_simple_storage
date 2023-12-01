1d2c14d03c23d9aa3f64d193a9e70d563d723275
AronLaptop <aron.cachago@gmail.com>
2023-12-01 15:52:36 -0500

    1.0.1

---
+++ scripts/deploy.py
@@ -0,0 +1,17 @@
+from brownie import accounts , config , SimpleStorage
+import os
+
+def deploy_simple_storage():
+    account = accounts.load("aron-account")
+    simple_storage = SimpleStorage.deploy({"from":account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_
+    storage.store(42 , {"from":account})
+    transaction.wait(1)
+    updated_store_value = simple_storage.retrieve()
+    print(updated_store_value)
+
+
+def main():
+    deploy_simple_storage()
