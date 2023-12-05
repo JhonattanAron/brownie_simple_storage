660db0ecc56c69389a38aaf5196359ebd94b72ba
AronLaptop <aron.cachago@gmail.com>
2023-12-05 14:12:15 -0500

    1.0.7

---
+++ tests/test_deploy_simple_storage.py
@@ -0,0 +1,29 @@
+from brownie import accounts , SimpleStorage
+
+def test_deploy():
+    # Arrage -> Organizar o arreglar
+    account = accounts[0]
+    # Act-> hacer algo
+    simple_storage = SimpleStorage.deploy({"from": account})
+    starting_value = simple_storage.retrieve()
+    expected = 0
+    # Assert -> Comprobar que a ido bien
+    assert starting_value == expected
+
+
+def test_updating_storage():
+    # * Arrage
+    account = accounts[0]
+    # * Act
+    simple_storage = SimpleStorage.deploy({"from":account})
+    txn = simple_storage.store(42, {"from":account})
+    txn.wait(1)
+    expected = 42
+    print(simple_storage.retrieve())
+    # * Assert
+    assert simple_storage.retrieve() == expected
+
+
+
+
+    
